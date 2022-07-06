0eff6412329b55d48cabc8f8d2a429cf9f28057e
Pugazhprince <89248877+Pugazhprince@users.noreply.github.com>
2022-07-07 00:10:35 +0530

    Adding Authentication Token for API

--- api/views.py
+++ api/views.py
@@ -4,9 +4,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from base.models import StudentData
 from .serializers import StudentDataSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class StudentApiView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         studentdata = StudentData.objects.all()
         serializer = StudentDataSerializer(studentdata, many=True)
